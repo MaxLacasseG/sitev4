39394ff99477d3bad08402c10e7e73e603ff1393
MaxLacasseG <max.lacasse.g@gmail.com>
2019-02-10 17:39:37 -0500

    Page accueil

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import AnimatedBg from "./home/AnimatedBg";
 export default class Home extends Component {
     render() {
         return (
             <div className="home">
                 <AnimatedBg />
+                <div className="logo-container">
+                    <img src="/img/logoNomCoul@3x.png" alt="logo" className="logo pt-5" />
+                    <ul className="nav pt-5">
+                        <li className="nav-item">
+                            <Link to="/web">Web</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/jeux-video">Jeux vidéo</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/creations">Autres créations</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/bio">Bio</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
